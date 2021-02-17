0d7b6d323f373d176568ce94f86e2c602b966717
Seth Bradshaw <seth.bradshaw27@gmail.com>
2021-02-17 11:20:53 -0700

    added get all modules endpoint

--- src/main/java/com/lambdaschool/oktafoundation/controllers/ModuleController.java
+++ src/main/java/com/lambdaschool/oktafoundation/controllers/ModuleController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RequestMapping(value = "/modules")
 @RestController
@@ -16,21 +17,34 @@ public class ModuleController {
     private ModuleService moduleService;
 
     @GetMapping(value = "/module/{moduleid}", produces = "application/json")
-    public ResponseEntity<?> fetchSingleModule(@PathVariable long moduleid) throws Exception {
+    public ResponseEntity<?> fetchSingleModule(@PathVariable long moduleid) throws Exception
+    {
         Module module = moduleService.fetchModuleById(moduleid);
 
         return new ResponseEntity<>(module, HttpStatus.OK);
     }
+
+    @GetMapping(value = "/modules", produces = "application/json")
+    public ResponseEntity<?> fetchingAllModules()
+    {
+        List<Module> moduleList = moduleService.fetchAllModules();
+
+        return new ResponseEntity<>(moduleList, HttpStatus.OK);
+    }
+
     @PostMapping(value = "/modules", consumes = "application/json")
-    public ResponseEntity<?> postModule(@RequestBody @Valid Module newModule){
+    public ResponseEntity<?> postModule(@RequestBody @Valid Module newModule)
+    {
         newModule.setModuleId(0);
         moduleService.save(newModule);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
+    
     @PutMapping(value = "/module/{moduleid}", consumes = "application/json")
     public ResponseEntity<?> putModule(@PathVariable long moduleid,
-                                       @RequestBody @Valid Module updatedModule){
+                                       @RequestBody @Valid Module updatedModule)
+    {
         updatedModule.setModuleId(moduleid);
         moduleService.save(updatedModule);
 
@@ -47,7 +61,8 @@ public class ModuleController {
     }
 
     @DeleteMapping(value = "/module/{moduleid}")
-    public ResponseEntity<?> deleteSingleModule(@PathVariable long moduleid){
+    public ResponseEntity<?> deleteSingleModule(@PathVariable long moduleid)
+    {
         moduleService.deleteModuleById(moduleid);
 
         return new ResponseEntity<>(HttpStatus.OK);
--- src/main/java/com/lambdaschool/oktafoundation/services/ModuleService.java
+++ src/main/java/com/lambdaschool/oktafoundation/services/ModuleService.java
@@ -2,10 +2,14 @@ package com.lambdaschool.oktafoundation.services;
 
 import com.lambdaschool.oktafoundation.models.Module;
 
+import java.util.List;
+
 public interface ModuleService {
     Module fetchModuleById(long moduleid) throws Exception;
     Module save(Module module);
     void deleteModuleById(long moduleid);
 
    Module edit(Module partiallyEditedModule);
+
+    List<Module> fetchAllModules();
 }
